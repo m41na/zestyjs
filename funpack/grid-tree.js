ab5fe0b6018c1b2c95fb27c43bf30b1d0a7cbba1
Stephen Maina <m41na@yahoo.com>
2018-08-12 16:25:02 -0500

    trying a different approach for observing model

---
+++ funpack/grid-tree.js
@@ -0,0 +1,22 @@
+class GridTree {
+
+    constructor(){
+
+    }
+
+    insert(){
+
+    }
+}
+
+
+
+class TreeNode {
+
+    constructor(){
+        this.value;
+        this.left;
+        this.right;
+    }
+}
+
