74a00cb244c6e6e9f5ce5ed555e98fbf37ae210e
Stephen Maina <m41na@yahoo.com>
2018-08-14 17:04:04 -0500

    slugging away at this reactivity thing

---
+++ blog-home/js/blog-home.js
@@ -0,0 +1,16 @@
+let data = {
+    navigation: ['Home','About','Services','Contact'],
+    page: {
+        title: 'Page Heading',
+        subtitle: 'Secondary Text',
+        blogs: [
+            {
+                img: '',
+                title: 'Post Title',
+                summary: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+                postdate: 'January 1, 2017',
+                author: 'Steve M'
+            }
+        ]
+    }
+}
